cbfb7a6b158056c6c911b39682996601be5b33f8
sonyarubakha <sonya.rubakha@gmail.com>
2026-05-03 22:46:00 +0300

    Recreated the code(class Treenode for node of the tree and class Solution with invertTree function) after watching a video on how to invert a binary tree using recursion tep by step on each subtree

---
+++ Module 1/Video practise/invert_binary_tree.py
@@ -0,0 +1,25 @@
+'''Module to invert binary tree.'''
+
+class Treenode:
+    '''
+    Definition for a binary tree node.
+    '''
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    '''
+    class with solution on how to inert binary tree.
+    '''
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        '''
+        Inverts binary tree.
+        '''
+        if root is None:
+            return None
+        root.left, root.right = root.right, root.left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
